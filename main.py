0892cfcb50ece3e89cd67289738432881379ced5
Woosung Lim <wml5207@psu.edu>
2020-09-15 15:23:59 +0000

    12

--- main.py
+++ main.py
@@ -6,17 +6,19 @@
 # Breakout: 6
 
 def sum_n(n):
-  if n <= 0:
+  if n == 0:
     return 0
+  if n == 1:
+    return 1
   else:
     return n + sum_n(n-1)
 
 def print_n(s,n):
-  if n <= 1:
-    print(s)
-  else:
-    print(s) 
-    print_n(s,n-1)
+  if n == 0:
+    return 
+ 
+  print(s) 
+  print_n(s,n-1)
   
 num=int(input("Enter an int: "))
 print(f"sum is {sum_n(num)}.")
